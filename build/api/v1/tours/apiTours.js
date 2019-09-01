94cf091c0d60706472f72dc227d992d097c412e9
Igor <igor_pavlenko@yahoo.com>
2019-09-01 07:41:22 +0200

    create recepie

--- build/api/v1/tours/apiTours.js
+++ build/api/v1/tours/apiTours.js
@@ -10,11 +10,13 @@ const apiUploadImage_1 = require("./apiUploadImage");
 const apiGetTours_1 = require("./apiGetTours");
 const bodyParser_1 = require("../general/bodyParser");
 exports.toursRouter = express_1.Router();
-exports.toursRouter.route("/")
+exports.toursRouter
+    .route('/')
     .get(apiCheckTourFilters_1.apiCheckTourFilters, apiGetTours_1.apiGetTours)
     .post(bodyParser_1.jsonParser, apiCreateTour_1.apiCreateTour);
-exports.toursRouter.route("/:id")
+exports.toursRouter
+    .route('/:id')
     .get(apiGetTourDetail_1.apiGetTourDetail)
     .delete(apiDeleteTour_1.apiDeleteTour)
     .patch(bodyParser_1.jsonParser, apiUpdateTour_1.apiUpdateTour);
-exports.toursRouter.post("/:id/img", apiUploadImage_1.apiUploadImage);
+exports.toursRouter.post('/:id/img', apiUploadImage_1.apiUploadImage);
--- build/api/v2/recepies/apiRecepies.js
+++ build/api/v2/recepies/apiRecepies.js
@@ -2,5 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const apiGetRecepies_1 = require("./apiGetRecepies");
+const bodyParser_1 = require("../../v1/general/bodyParser");
+const apiCreateRecepie_1 = require("./apiCreateRecepie");
 exports.recepiesRouter = express_1.Router();
-exports.recepiesRouter.get('/', apiGetRecepies_1.apiGetRecepies);
+exports.recepiesRouter
+    .route('/')
+    .get(apiGetRecepies_1.apiGetRecepies)
+    .post(bodyParser_1.jsonParser, apiCreateRecepie_1.apiCreateRecepie);
